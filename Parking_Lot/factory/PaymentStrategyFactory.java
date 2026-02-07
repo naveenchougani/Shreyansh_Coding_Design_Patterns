c46d782e511f9ddce47ce23f9ae16c76b0f9742b
naveenchougani <69831778+naveenchougani@users.noreply.github.com>
2026-02-07 14:43:06 +0530

    Refactor get method to use switch expression

--- Parking_Lot/factory/PaymentStrategyFactory.java
+++ Parking_Lot/factory/PaymentStrategyFactory.java
@@ -1,7 +1,7 @@
 public class PaymentStrategyFactory{
 
   public static PaymentStrategy get(PaymentMode mode){
-    return switch(mode)->{
+    return switch(mode){
       case UPI -> new UpiPayment();
       case CASH -> new CashPayment();
       case CARD -> new CardPayment();
